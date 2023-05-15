cac29d0d161aaa7b55db63776bc399ca4029bd55
matteorocco <matealbe.rocco@gmail.com>
2023-05-15 14:38:06 +0200

    test

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -70,4 +70,110 @@ public class RomanPrinterTest {
 		
 		assertEquals("IX", romanNumber);
 	}
+	
+	@Test
+	public void Test10() {
+		String romanNumber = IntegerToRoman.convert(10);
+		
+		assertEquals("X", romanNumber);
+	}
+	
+	@Test
+	public void Test50() {
+		String romanNumber = IntegerToRoman.convert(50);
+		
+		assertEquals("L", romanNumber);
+	}
+
+	@Test
+	public void Test100() {
+		String romanNumber = IntegerToRoman.convert(100);
+		
+		assertEquals("C", romanNumber);
+	}
+
+	@Test
+	public void Test500() {
+		String romanNumber = IntegerToRoman.convert(500);
+		
+		assertEquals("D", romanNumber);
+	}
+
+	@Test
+	public void Test1000() {
+		String romanNumber = IntegerToRoman.convert(1000);
+		
+		assertEquals("M", romanNumber);
+	}
+
+	@Test
+	public void Test31() {
+		String romanNumber = IntegerToRoman.convert(31);
+		
+		assertEquals("XXXI", romanNumber);
+	}
+
+	@Test
+	public void Test148() {
+		String romanNumber = IntegerToRoman.convert(148);
+		
+		assertEquals("CXLVIII", romanNumber);
+	}
+
+	@Test
+	public void Test294() {
+		String romanNumber = IntegerToRoman.convert(294);
+		
+		assertEquals("CCXCIV", romanNumber);
+	}
+	
+	@Test
+	public void Test312() {
+		String romanNumber = IntegerToRoman.convert(312);
+		
+		assertEquals("CCCXII", romanNumber);
+	}
+	
+	@Test
+	public void Test421() {
+		String romanNumber = IntegerToRoman.convert(421);
+		
+		assertEquals("CDXXI", romanNumber);
+	}
+	
+	@Test
+	public void Test528() {
+		String romanNumber = IntegerToRoman.convert(528);
+		
+		assertEquals("DXXVIII", romanNumber);
+	}
+	
+	@Test
+	public void Test621() {
+		String romanNumber = IntegerToRoman.convert(621);
+		
+		assertEquals("DCXXI", romanNumber);
+	}
+	
+	@Test
+	public void Test782() {
+		String romanNumber = IntegerToRoman.convert(782);
+		
+		assertEquals("DCCLXXXII", romanNumber);
+	}
+	
+	@Test
+	public void Test870() {
+		String romanNumber = IntegerToRoman.convert(870);
+		
+		assertEquals("DCCCLXX", romanNumber);
+	}
+	
+	@Test
+	public void Test941() {
+		String romanNumber = IntegerToRoman.convert(941);
+		
+		assertEquals("CMXLI", romanNumber);
+	}
+	
 }
